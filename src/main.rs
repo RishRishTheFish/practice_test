4eb0652aa2ed5352a5a0937ab49ed113df3ba94f
rishay.dutt <rishay.dutt@student.onslow.school.nz>
2025-04-09 19:03:08 +1200

    added comments

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use std::{collections::HashMap, error::Error, fmt::Display, io::{stdin, stdout, 
 use std::fs;
 
 fn ask_question() -> String {
+    // Constructs a new string for the awnser to be put into
     let mut awnser = String::new();
+    // Flush stdout so previous input is not detected
     stdout().flush().unwrap();
+    // Reads the actual line (from intput) and also provide error handling
     if let Err(error) = stdin().read_line(&mut awnser) {
         println!("test Error: {error}");
     }
@@ -11,12 +14,18 @@ fn ask_question() -> String {
     awnser
 }
 
-#[derive(PartialEq, serde::Deserialize, Debug, Clone)]
+// Derive macros so i can compare classifications with other ones, debug it, and clone the data
+// to avoid memory issues (since preformance loss is acceptable)
+#[derive(PartialEq, Debug, Clone)]
+// Classification will take a simple string as a name, and a optional, parent classification which is 
+// optional for the sake of orphan nodes and when a node is first initilized
+// and a box for classification so the data can be consistently borrowed as mutable
 struct Classification {
     name: String,
     parent_classification: Option<Box<Classification>>
 }
 
+// Implimentation of Classification which provides a constructer funnction
 impl Classification {
     fn new(name: String, parent_classification: Classification) -> Classification {
         Classification {
@@ -25,12 +34,19 @@ impl Classification {
         }
     }
 }
+
+// RawBird will be what will be immediately derived from the json file
+// as deserialization will not be possible for parentNodes which is a property of Bird with the properties I want
+// as in, a classification which is supposed to be a parent node, will have a name and a optional refrence to other parent nodes
+// which is not possible with raw json. 
 #[derive(serde::Deserialize, Clone)]
 struct RawBird {
     name: String,
     commonName: String,
     parentNodes: Vec<String>
 }
+
+// A implimentation for RawBird that provides a simple constructer function
 impl RawBird {
     fn new(name: String, parentNodes: Vec<String>, commonName: String) -> RawBird {
         RawBird {
@@ -40,6 +56,9 @@ impl RawBird {
         }
     }
 }
+
+// here is a From(RawBird) implimentation to convert the raw json struct into a normal bird struct
+// with the only diffrence being that parent nodes will be turned from simple strings into classifications
 impl From<RawBird> for Bird {
     fn from(raw: RawBird) -> Self {
         Bird {
@@ -54,12 +73,14 @@ impl From<RawBird> for Bird {
     }
 }
 
+//Macros for PartialEq and Debug for equating a bird with another and printing it out
 #[derive(PartialEq, Debug)]
 struct Bird {
     name: String,
     commonName: String,
     parentNodes: Vec<Classification>
 }
+// Basic implimentation of bird with a constructer function
 impl Bird {
     fn new(name: String, parentNodes: Vec<Classification>, commonName: String) -> Bird {
         Bird {
@@ -69,10 +90,16 @@ impl Bird {
         }
     }
 }
+
+// Collection is everything contained in a single struct, this makes it easier to refer to the data
+// and to better originize certain functions
 struct Collection<'a> {
     classifications: Vec<&'a Classification>,
     birds: Vec<Bird>
 }
+// The implimentation for Collection which will provide a constructer function, while also providing functions
+// for ensuring there are no duplicate classifications, or birds, and one where it will add a classification if none exists,
+// and return the refrence (regardless if it existed prior or not)
 impl Collection<'_> {
     fn new<'a>() -> Collection<'a> {
         Collection { classifications: vec![], birds: vec![] }
@@ -104,14 +131,20 @@ impl Collection<'_> {
     }
 }
 
-
 fn main() {
+    // Main function, it will run all my code, first thing it does is that it initilizes the collection
+    // Then using fs it will read the birdData json file, (with error handling), captured as a string
+    // next it is deserilized into Vec<RawBird> (vector of rawbirds) because rawbirds is more accurate to the JSON data
+    // then it is converted into a final struct, by iterating over all the birds gained from the json file.
+    // using Bird::from it will convert it into regular birds 
     let mut collection = Collection::new();
     let raw_bird_data: String  = fs::read_to_string("C:\\Users\\DuttR\\school\\practice_test\\src\\birdData.json").expect("Failed");
     let raw_bird_json: Vec<RawBird> = serde_json::from_str(&raw_bird_data).expect("Incorrect json");
     let final_bird_json: Vec<Bird> = raw_bird_json.iter().map(|bird| Bird::from(bird.clone())).collect();
     
+    // Loops until the user decides to exist
     loop {
+        // Welcome screen, what the user sees when they run the program, options are listed below
         println!("Welcome to the Bird Collection!");
         println!("###############################");
         println!("(a) Addes a bird");
@@ -119,10 +152,15 @@ fn main() {
         println!("(c) prints out all classes");
         println!("(t) Adds a bird to a class");
         println!("(k) Adds a class");
+        println!("(j) Search for a bird by scientific name");
+        println!("(m) Searches for a bird by common name");
         println!("(q) Quits");
+        // asks the user what operation they want to do
         let operation = ask_question();
+        // Lowercases it, then trims it, before matching it for all operation
         match operation.to_lowercase().as_str().trim() {
             "a" => {
+                // For adding birds, it will ask the two questions mentioned, then simply push a newly constructed bird into the collection
                 println!("Whats the name of the bird do you want to add?");
                 let bird_name = ask_question();
                 println!("Whats the scientific name of the bird?");
@@ -132,6 +170,9 @@ fn main() {
                 );
             },
             "p" => {
+                // for printing all the birds, simply loop all of the birds, for printing the final string from the parentNodes, 
+                // you use String::from_iter which basically concats a iteratable, I then make a iteratable from the birds parentNodes
+                // and get the node names and add a seperator
                 for bird in &final_bird_json {
                     println!("name: {}, common name: {}, species: {}", bird.name, bird.commonName, 
                         String::from_iter(
@@ -140,6 +181,12 @@ fn main() {
                 }
             },
             "c" => {
+                // For classifications it is more complicated, since they are within all birds
+                // you have to loop over all birds while avoiding duplicate refrences, you can do this by
+                // using fold, which keeps track of previously iterated parentNode names (within acc)
+                // within fold i made a varible called unique_refrences which will filter unique_classifications for unique refrences
+                // using .any as the check, which just checks if there has been any previous matches with anything in acc
+                // acc is then added with the unique refences, then returned
                 for classification in final_bird_json.iter().fold(
                     vec![], |mut acc, bird| {
                         let unique_refrences: Vec<Classification> = bird.parentNodes
@@ -157,6 +204,12 @@ fn main() {
             },
             "t" => {},
             "k" => {},
+            "j" => {
+
+            },
+            "m" => {
+
+            },
             "q" => break,
             _ => {
                 println!("Non-existent operation")
