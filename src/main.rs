62ee2d438250a309a55f1e68357e5bb7b3ebf988
rishay.dutt <rishay.dutt@student.onslow.school.nz>
2025-04-11 18:17:24 +1200

    latest

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashMap, error::Error, fmt::Display, io::{stdin, stdout, Write}, vec};
+use std::{cell::RefCell, collections::HashMap, hash::Hash, io::{stdin, stdout, Write}, vec};
 use std::fs;
 
+/// A function to ask the question, error handle, and trim it
 fn ask_question() -> String {
     // Constructs a new string for the awnser to be put into
     let mut awnser = String::new();
@@ -8,114 +9,131 @@ fn ask_question() -> String {
     stdout().flush().unwrap();
     // Reads the actual line (from intput) and also provide error handling
     if let Err(error) = stdin().read_line(&mut awnser) {
-        println!("test Error: {error}");
+        println!("Error: {error}");
     }
     println!("");
-    awnser
+    awnser.trim().to_string()
 }
 
 // Derive macros so i can compare classifications with other ones, debug it, and clone the data
 // to avoid memory issues (since preformance loss is acceptable)
 #[derive(PartialEq, Debug, Clone)]
-// Classification will take a simple string as a name, and a optional, parent classification which is 
-// optional for the sake of orphan nodes and when a node is first initilized
-// and a box for classification so the data can be consistently borrowed as mutable
+/// Classification will take a simple string as a name, and a optional, parent classification which is 
+/// optional for the sake of orphan nodes and when a node is first initilized
+/// and a box for classification so the data can be consistently borrowed as mutable
 struct Classification {
     name: String,
     parent_classification: Option<Box<Classification>>
 }
 
-// Implimentation of Classification which provides a constructer funnction
 impl Classification {
-    fn new(name: String, parent_classification: Classification) -> Classification {
+    fn new(name: String, parent_classification: Option<Classification>) -> Classification {
         Classification {
             name,
-            parent_classification: Some(Box::new(parent_classification)),
+            parent_classification: {
+                if parent_classification.is_none(){
+                    None
+                } else {
+                    Some(Box::new(parent_classification.unwrap()))
+                }
+            }
         }
     }
 }
 
-// RawBird will be what will be immediately derived from the json file
-// as deserialization will not be possible for parentNodes which is a property of Bird with the properties I want
-// as in, a classification which is supposed to be a parent node, will have a name and a optional refrence to other parent nodes
-// which is not possible with raw json. 
+/// RawBird will be what will be immediately derived from the json file
+/// as deserialization will not be possible for parent_nodes which is a property of Bird with the properties I want
+/// as in, a classification which is supposed to be a parent node, will have a name and a optional refrence to other parent nodes
+/// which is not possible with raw json. 
 #[derive(serde::Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
 struct RawBird {
     name: String,
-    commonName: String,
-    parentNodes: Vec<String>
+    common_name: String,
+    parent_nodes: Vec<String>
 }
 
-// A implimentation for RawBird that provides a simple constructer function
-impl RawBird {
-    fn new(name: String, parentNodes: Vec<String>, commonName: String) -> RawBird {
-        RawBird {
-            name, 
-            parentNodes,
-            commonName,
-        }
-    }
-}
 
-// here is a From(RawBird) implimentation to convert the raw json struct into a normal bird struct
-// with the only diffrence being that parent nodes will be turned from simple strings into classifications
 impl From<RawBird> for Bird {
+    // here is a From(RawBird) implimentation to convert the raw json struct into a normal bird struct
+    // with the only diffrence being that parent nodes will be turned from simple strings into classifications
     fn from(raw: RawBird) -> Self {
         Bird {
             name: raw.name,
-            commonName: raw.commonName,
-            parentNodes: raw.parentNodes
+            common_name: raw.common_name,
+            parent_nodes: raw.parent_nodes
                 .iter()
-                .map(|item| Classification {
+                .map(|item| RefCell::new(Classification {
                      name: item.to_string(), parent_classification: None
-                }).collect(),
+                })).collect(),
         }
     }
 }
 
-//Macros for PartialEq and Debug for equating a bird with another and printing it out
-#[derive(PartialEq, Debug)]
+/// Macros for PartialEq and Debug for equating a bird with another and printing it out, and Clone for avoiding borrowing issues
+#[derive(PartialEq, Debug, Clone)]
 struct Bird {
     name: String,
-    commonName: String,
-    parentNodes: Vec<Classification>
+    common_name: String,
+    parent_nodes: Vec<RefCell<Classification>>
 }
-// Basic implimentation of bird with a constructer function
+
 impl Bird {
-    fn new(name: String, parentNodes: Vec<Classification>, commonName: String) -> Bird {
+    fn new(name: String, parent_nodes: Vec<RefCell<Classification>>, common_name: String) -> Bird {
         Bird {
             name, 
-            parentNodes,
-            commonName,
+            parent_nodes,
+            common_name,
         }
     }
 }
 
-// Collection is everything contained in a single struct, this makes it easier to refer to the data
-// and to better originize certain functions
-struct Collection<'a> {
-    classifications: Vec<&'a Classification>,
+/// Collection is everything contained in a single struct, this makes it easier to refer to the data
+/// and to better originize certain functions
+struct Collection {
+    classifications: Vec<RefCell<Classification>>,
     birds: Vec<Bird>
 }
-// The implimentation for Collection which will provide a constructer function, while also providing functions
-// for ensuring there are no duplicate classifications, or birds, and one where it will add a classification if none exists,
-// and return the refrence (regardless if it existed prior or not)
-impl Collection<'_> {
-    fn new<'a>() -> Collection<'a> {
+/// The implimentation for Collection which will provide a constructer function, while also providing functions
+/// for ensuring there are no duplicate classifications, or birds, and one where it will add a classification if none exists,
+/// and return the refrence (regardless if it existed prior or not)
+impl Collection {
+    fn new() -> Collection {
         Collection { classifications: vec![], birds: vec![] }
     }
-    fn add_or_return_classification_ref<'a>(&'a mut self, classification: &'static Classification) -> &Classification {
-        if !self.classifications.iter().any(|c| **c == *classification) {
-            self.classifications.push(&classification);
+    fn search_bird_by_name(&self, name: String) -> Vec<&Bird> {
+        let mut matched_birds = vec![];
+        for bird in &self.birds {
+            if bird.name.contains(&name){
+                matched_birds.push(bird);
+            }
         }
-        self.classifications
-            .iter()
-            .find(|c| c.name == classification.name)
-            .unwrap()
+        matched_birds
     }
-    fn add_and_check_for_duplicates_classifications<'a>(&'a mut self, classification: &'static Classification) -> Option<usize> {
-        if !self.classifications.iter().any(|c| c.name == classification.name) {
-            self.classifications.push(&classification);
+    fn search_bird_by_common_name(&self, name: String) -> Vec<&Bird> {
+        let mut matched_birds = vec![];
+        for bird in &self.birds {
+           println!("{}, {}", bird.name, name);
+            if bird.common_name.contains(&name){
+                matched_birds.push(bird);
+            }
+        }
+        matched_birds
+    }
+    // fn add_or_return_classification_ref(&mut self, classification: Classification) -> &RefCell<Classification> {
+    //     let name = classification.name.clone(); 
+    //     if !self.classifications.iter().any(|c| c.borrow().name == name) {
+    //         self.classifications.push(RefCell::new(classification));
+    //     }
+    
+    //     self.classifications
+    //         .iter()
+    //         .find(|c| c.borrow().name == name)
+    //         .unwrap()
+    // }
+    fn add_and_check_for_duplicates_classifications<'a>(&'a mut self, classification: &Classification) -> Option<usize> {
+        if !self.classifications.iter().any(|c| c.borrow().name == classification.name) {
+            self.classifications.push(RefCell::new(classification.clone()));
             Some(self.classifications.len())
         } else {
             None
@@ -130,18 +148,32 @@ impl Collection<'_> {
             }
     }
 }
+fn sort_by_classification(birds: Vec<Bird>) -> Vec<Bird> {
+    let final_birds = vec![];
+    for bird in birds {
+        let acc: HashMap<Classification, usize> = HashMap::new();
+        for unborrowed_classification in bird.parent_nodes {
+            let classification = unborrowed_classification.borrow();
+            
+        }
+    }
+    final_birds
+}
 
+/// Main function, it will run all my code, first thing it does is that it initilizes the collection
+/// Then using fs it will read the birdData json file, (with error handling), captured as a string
+/// next it is deserilized into Vec<RawBird> (vector of rawbirds) because rawbirds is more accurate to the JSON data
+/// then it is converted into a final struct, by iterating over all the birds gained from the json file.
+/// using Bird::from it will convert it into regular birds 
 fn main() {
-    // Main function, it will run all my code, first thing it does is that it initilizes the collection
-    // Then using fs it will read the birdData json file, (with error handling), captured as a string
-    // next it is deserilized into Vec<RawBird> (vector of rawbirds) because rawbirds is more accurate to the JSON data
-    // then it is converted into a final struct, by iterating over all the birds gained from the json file.
-    // using Bird::from it will convert it into regular birds 
     let mut collection = Collection::new();
-    let raw_bird_data: String  = fs::read_to_string("C:\\Users\\DuttR\\school\\practice_test\\src\\birdData.json").expect("Failed");
+    let raw_bird_data: String  = fs::read_to_string("birdData.json").expect("Failed");
     let raw_bird_json: Vec<RawBird> = serde_json::from_str(&raw_bird_data).expect("Incorrect json");
-    let final_bird_json: Vec<Bird> = raw_bird_json.iter().map(|bird| Bird::from(bird.clone())).collect();
-    
+    let mut final_bird_json: Vec<Bird> = raw_bird_json.iter().map(|bird| Bird::from(bird.clone())).collect();
+    final_bird_json = sort_by_classification(final_bird_json);
+    collection.birds.extend(final_bird_json.clone());
+
+
     // Loops until the user decides to exist
     loop {
         // Welcome screen, what the user sees when they run the program, options are listed below
@@ -152,8 +184,9 @@ fn main() {
         println!("(c) prints out all classes");
         println!("(t) Adds a bird to a class");
         println!("(k) Adds a class");
-        println!("(j) Search for a bird by scientific name");
+        println!("(j) Search for a bird by name");
         println!("(m) Searches for a bird by common name");
+        println!("(l) Searches for a bird by species");
         println!("(q) Quits");
         // asks the user what operation they want to do
         let operation = ask_question();
@@ -165,18 +198,19 @@ fn main() {
                 let bird_name = ask_question();
                 println!("Whats the scientific name of the bird?");
                 let bird_common_name = ask_question();
-                collection.birds.push(
-                    Bird { name: bird_name, commonName: bird_common_name, parentNodes: vec![] }
-                );
+                let bird_to_add = collection.add_and_check_for_duplicates_bird(Bird::new(bird_name, vec![], bird_common_name));
+                if bird_to_add.is_none() {
+                    println!("That bird already exists")
+                }
             },
             "p" => {
-                // for printing all the birds, simply loop all of the birds, for printing the final string from the parentNodes, 
-                // you use String::from_iter which basically concats a iteratable, I then make a iteratable from the birds parentNodes
+                // for printing all the birds, simply loop all of the birds, for printing the final string from the parent_nodes, 
+                // you use String::from_iter which basically concats a iteratable, I then make a iteratable from the birds parent_nodes
                 // and get the node names and add a seperator
                 for bird in &final_bird_json {
-                    println!("name: {}, common name: {}, species: {}", bird.name, bird.commonName, 
+                    println!("name: {}, common name: {}, species: {}", bird.name, bird.common_name, 
                         String::from_iter(
-                            bird.parentNodes.iter().map(|node| node.name.clone() + " ")
+                            bird.parent_nodes.iter().map(|node| node.borrow().name.clone() + " ")
                         ))
                 }
             },
@@ -189,12 +223,12 @@ fn main() {
                 // acc is then added with the unique refences, then returned
                 for classification in final_bird_json.iter().fold(
                     vec![], |mut acc, bird| {
-                        let unique_refrences: Vec<Classification> = bird.parentNodes
+                        let unique_refrences: Vec<Classification> = bird.parent_nodes
                             .iter()
+                            .map(|unique_classification| unique_classification.borrow().clone())
                             .filter(|unique_classificaion|
-                                !acc.iter().any(|matching_classification: &Classification| matching_classification == *unique_classificaion)
+                                !acc.iter().any(|matching_classification: &Classification| *matching_classification == *unique_classificaion)
                                 )
-                                .cloned()
                                 .collect();
                             acc.extend(unique_refrences);
                             acc
@@ -202,13 +236,39 @@ fn main() {
                     println!("{}", classification)
                 }
             },
-            "t" => {},
-            "k" => {},
+            "t" => {
+                
+            },
+            "k" => {
+                println!("Whats the name of the species you want to add? (taxaomic grouping");
+                let species_name = ask_question();
+                let species_to_add = collection.add_and_check_for_duplicates_classifications(&Classification::new(species_name, None));
+                if species_to_add.is_none() {
+                    println!("Error: Duplicate species detected")
+                }
+            },
             "j" => {
-
+                println!("What is the name (it will work with partial input)");
+                let name = ask_question();
+                let birds = collection.search_bird_by_name(name);
+                for bird in birds.iter() {
+                    println!("name: {}, common name: {}, species: {}", bird.name, bird.common_name, 
+                        String::from_iter(
+                            bird.parent_nodes.iter().map(|node| node.borrow().name.clone() + " ")
+                        ))
+                }
             },
             "m" => {
-
+                println!("What is the common name (it will work with partial input)");
+                let name = ask_question();
+                let birds = collection.search_bird_by_common_name(name);
+                println!("Matched birds:");
+                for bird in &birds {
+                    println!("name: {}, common name: {}, species: {}", bird.name, bird.common_name, 
+                        String::from_iter(
+                            bird.parent_nodes.iter().map(|node| node.borrow().name.clone() + " ")
+                        ))
+                }
             },
             "q" => break,
             _ => {
